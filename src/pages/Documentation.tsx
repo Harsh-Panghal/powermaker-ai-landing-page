892a3bf76059918e3677766f3b9e85eafca72b76
Harsh Choudhary <harsh.choudhary.glb@gmail.com>
2025-10-27 12:19:47 +0530

    hide unnecessary buttons from the  documentation page

--- src/pages/Documentation.tsx
+++ src/pages/Documentation.tsx
@@ -1629,14 +1629,14 @@ export default function Documentation() {
                   <ArrowRight className="w-5 h-5 ml-2 group-hover:translate-x-1 transition-transform" />
                 </Button>
 
-                <Button
+                {/* <Button
                   size="lg"
                   variant="outline"
                   className="bg-white/10 border-white/20 text-white hover:bg-white/20 backdrop-blur-sm"
                 >
                   <ExternalLink className="w-5 h-5 mr-2" />
                   View API Reference
-                </Button>
+                </Button> */}
               </div>
             </div>
 
@@ -1846,13 +1846,13 @@ export default function Documentation() {
                 <Network className="w-5 h-5 mr-2" />
                 Contact Support
               </Button>
-              <Button
+              {/* <Button
                 size="lg"
                 variant="outline"
                 className="bg-white/10 border-white/20 text-white hover:bg-white/20 backdrop-blur-sm"
               >
                 Join Beta Waitlist
-              </Button>
+              </Button> */}
             </div>
           </div>
         </section>
